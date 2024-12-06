5d82f8fbb7aed352af7a4fd420c36dba9034cbbd
bhargavi0145 <bhargavipeddiboina2001@gmail.com>
2024-12-06 15:42:44 +0530

    completed

---
+++ script.js
@@ -0,0 +1,14 @@
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    const amPm = hours >= 12 ? 'PM' : 'AM';
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes
+        .toString()
+        .padStart(2, '0')}:${seconds.toString().padStart(2, '0')} :${amPm}`;
+    
+      document.getElementById('time').textContent = formattedTime;
+}
+setInterval(updateClock,1000)
+updateClock()
